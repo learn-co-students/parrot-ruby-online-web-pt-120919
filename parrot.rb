aaaa8b9ef0e30e960e10de12dd4939f804737165
travis Lawson <travislawclimb@gmail.com>
2019-12-13 20:15:42 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+#what are arguments
+
+#methods are boxes that encapsulate some type of logic
+
+def parrot(phrase = "Squawk!") 
+  puts phrase
+  return phrase
+end
+
+parrot
