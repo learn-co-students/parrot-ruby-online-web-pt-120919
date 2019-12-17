9eb604fa4a07570a6c325e2736ea8d24f41babc4
delightful-torrent-1430 <leadsultan8x@gmail.com>
2019-12-17 03:28:56 +0000

    Done.

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,6 +1,11 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
+def parrot (phrase = "Squawk!")
+  puts phrase
+  return phrase
+end
+
 describe '#parrot' do
   it 'should output "Squawk!" to the terminal when called without any arguments' do
     expect($stdout).to receive(:puts).with("Squawk!")
@@ -14,10 +19,12 @@ describe '#parrot' do
 
   it 'should output the given phrase when called with an argument' do
     expect($stdout).to receive(:puts).with("Pretty bird!")
+   
     parrot("Pretty bird!")
   end
 
   it 'should return the given phrase when called with an argument' do
+    
     phrase = parrot("Pretty bird!")
 
     expect(phrase).to eq("Pretty bird!")
